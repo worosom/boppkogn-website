7ea2acdaaf7721ce7fe57ac73c5bd77f8b241af0
Alexander Morosow <alexander.morosow@gmail.com>
2020-10-17 18:20:16 +0200

    add global back button on subsites

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,9 @@
 <template>
   <section class="main_wrap">
     <bk-logo :slim="true"/>
+    <b-btn title="Back" :to="'/'" class="main-back-btn">
+      <md-arrow-back-icon w="1.5rem" h="1.5rem"/>
+    </b-btn>
     <nuxt/>
     <bk-about/>
     <bk-partners/>
@@ -9,9 +12,30 @@
   </section>
 </template>
 
-<style>
+<style lang="scss">
 body {
   background: #FF0000;
+}
+.btn.main-back-btn {
+  z-index: 1;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 50px;
+  height: 50px;
+  background-color: #FFFF00;
+  color: #F00;
+  border: solid 5px red;
+  border-radius: 0;
+  box-sizing: border-box;
+  display: grid;
+  align-items: center;
+  justify-content: center;
+  padding-bottom: 6px;
+  &:hover, &:active:not(:disabled) {
+    animation: strobe 250ms infinite;
+    background-color: #00F;
+  }
 }
 </style>
 <script>
@@ -20,6 +44,7 @@ import BkAbout from '~/components/About'
 import BkPartners from '~/components/Partners'
 import BkContact from '~/components/Contact'
 import BkFooter from '../components/Footer';
+import MdArrowBackIcon from 'vue-ionicons/dist/md-arrow-back.vue'
 
 export default {
   components: {
@@ -27,7 +52,8 @@ export default {
     BkAbout,
     BkPartners,
     BkContact,
-    BkFooter
+    BkFooter,
+    MdArrowBackIcon
   }
 }
 </script>
