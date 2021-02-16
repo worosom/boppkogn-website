71b3b07fa3d37c311508e12c6a71a4e5ebb26ce7
Alexander Morosow <alexander.morosow@gmail.com>
2021-02-16 15:33:49 +0100

    adding plausible

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,7 @@ export default {
       { rel: 'stylesheet', type: 'text/css', href: '/webfonts/sporting_grotesque/font.css' }
     ],
     script: [
-      { async: true, defer: true, 'data-domain': 'bopp-kogn.africa', src: '/plausible.js' }
+      { async: true, defer: true, 'data-domain': 'bopp-kogn.africa', src: '/js/plausible.js' }
     ]
   },
   generate: {
